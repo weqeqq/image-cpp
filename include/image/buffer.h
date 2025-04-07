999eaac081858255c751f5cfe8b8a8632ad69886
weqeq <ggmodwear@gmail.com>
2025-04-07 22:55:56 +0300

    Rewrite buffer

--- include/image/buffer.h
+++ include/image/buffer.h
@@ -13,161 +13,153 @@
 #include <image/element.h>
 
 namespace Image {
-template <Depth::Tp depth = Depth::Eight, 
-          Color::Tp color = Color::RGB>
+
+template <Depth::Tp DepthV  = DefDepth, 
+          Color::Tp ColorV  = DefColor,
+          bool AlphaSetting = DisableAlpha>
 class Buffer {
 public:
+  static constexpr Depth::Tp     DepthValue   = DepthV;
+  static constexpr Color::Tp     ColorValue   = ColorV;
+  static constexpr bool          AlphaEnabled = AlphaSetting;
+  static constexpr std::uint64_t ChannelCount = Color::ChannelCount<ColorV, AlphaSetting>;
 
-  template <typename BufferTy>
-  class Iterator;
+  using UsedElement = Element<DepthV, ColorV, AlphaSetting>;
 
-  static constexpr Depth::Tp     depth_value   = depth;
-  static constexpr Color::Tp     color_value   = color;
-  static constexpr std::uint64_t channel_count = Color::ChannelCount<color>;
-  static constexpr std::uint64_t ElementBCount = Element<depth, color>::BCount;
+  static constexpr std::uint64_t ByteCountInElement = UsedElement::BCount;
+
+  using Container = std::vector<UsedElement>;
 
   Buffer() = default;
-
-  Buffer(std::uint64_t row_count, std::uint64_t column_count, std::vector<Element<depth, color>> data) 
+  Buffer(std::uint64_t row_count, std::uint64_t column_count, Container data) 
     : row_count_    (row_count) 
     , column_count_ (column_count) 
     , data_         (std::move(data)) {}
 
-  Buffer(std::uint64_t row_count, std::uint64_t column_count, Depth::Underlying<depth> value) 
+  using Value = typename UsedElement::Value;
+
+  Buffer(std::uint64_t row_count, std::uint64_t column_count, Value value) 
     : row_count_    (row_count)
     , column_count_ (column_count) 
-    , data_         (std::vector<Element<depth, color>>(row_count_ * column_count_, Element<depth, color>(value))) {}
+    , data_         (Container(row_count_ * column_count_, UsedElement(value))) {}
 
-  Buffer(std::uint64_t row_count, std::uint64_t column_count) : Buffer(row_count, column_count, Depth::Min<depth>) {}
+  Buffer(std::uint64_t row_count, std::uint64_t column_count) 
+    : Buffer(row_count, column_count, 0) {}
 
-  bool operator==(const Buffer<depth, color> &other) const {
+  bool operator==(const Buffer &other) const {
     return row_count_    == other.row_count_    && 
            column_count_ == other.column_count_ && 
            data_         == other.data_;
   }
-  bool operator!=(const Buffer<depth, color> &other) const {
+  bool operator!=(const Buffer &other) const {
     return !operator==(other);
   }
-
-  static Buffer<depth, color> From(
-    std::array<Buffer<depth, Color::Grayscale>, Color::ChannelCount<color>> channel_list
-  ) {
-    std::uint64_t row_count    = channel_list[0].GetRCount();
-    std::uint64_t column_count = channel_list[0].GetCCount();
-
-    Buffer<depth, color> output(row_count, column_count);
-
-    for (auto channel_index = 0u; 
-              channel_index < Color::ChannelCount<color>; 
-              channel_index++) {
-      for (auto index = 0u; 
-                index < output.GetLength();
-                index++) {
-        output[index][channel_index] = channel_list[channel_index][index];
-      }
-    }
-    return output;
-  }
-
-  std::uint64_t GetRCount() const {
+  std::uint64_t GetRowCount() const {
     return row_count_;
   }
-  std::uint64_t GetCCount() const {
+  std::uint64_t GetColumnCount() const {
     return column_count_;
   }
   std::uint64_t GetLength() const {
     return data_.size();
   }
-  std::uint64_t GetBCount() const {
-    return GetLength() * Element<depth, color>::bcount;
+  std::uint64_t GetByteCount() const {
+    return GetLength() * ByteCountInElement;
   }
   bool IsEmpty() const {
     return GetLength() == 0;
   }
 
-  Element<depth, color> &operator[](std::uint64_t index) {
+  UsedElement &operator[](std::uint64_t index) {
     return data_[index];
   }
-  const Element<depth, color> &operator[](std::uint64_t index) const {
+  const UsedElement &operator[](std::uint64_t index) const {
     return data_[index];
   }
-  Element<depth, color> &operator()(std::uint64_t row, std::uint64_t column) {
+  UsedElement &operator()(std::uint64_t row, std::uint64_t column) {
     return operator[](row * column_count_ + column);
   }
-  const Element<depth, color> &operator()(std::uint64_t row, std::uint64_t column) const {
+  const UsedElement &operator()(std::uint64_t row, std::uint64_t column) const {
     return operator[](row * column_count_ + column);
+  }
+
+  using Iterator      = typename Container::iterator;
+  using ConstIterator = typename Container::const_iterator;
+
+  using RevIterator      = typename Container::reverse_iterator;
+  using ConstRevIterator = typename Container::const_reverse_iterator;
+
+  auto Begin() {
+    return data_.begin();
+  }
+  auto Begin() const {
+    return data_.begin();
+  }
+  auto ConstBegin() const { 
+    return data_.cbegin();
+  }
+  auto End() {
+    return data_.end();
+  }
+  auto End() const {
+    return data_.end();
+  }
+  auto ConstEnd() const {
+    return data_.cend();
+  }
+  auto RevBegin() {
+    return data_.rbegin();
+  }
+  auto RevBegin() const {
+    return data_.rbegin();
+  }
+  auto ConstRevBegin() const {
+    return data_.crbegin();
+  }
+  auto RevEnd() {
+    return data_.rend();
+  }
+  auto RevEnd() const {
+    return data_.rend();
+  }
+  auto ConstRevEnd() const {
+    return data_.crend();
   }
 
 private:
   std::uint64_t row_count_;
   std::uint64_t column_count_;
-  std::vector<Element<depth, color>> data_;
 
+  Container data_;
 };
 
-template <Depth::Tp depth, Color::Tp color>
-template <typename BufferTy>
-class Buffer<depth, color>::Iterator {
-public:
-  explicit Iterator(BufferTy &container, std::uint64_t index) 
-    : container_(container) 
-    , index_(index) {}
-
-  std::conditional_t<!std::is_const_v<BufferTy>,
-          Element<depth, color>,
-    const Element<depth, color>> &
-  operator*() const {
-    return container_[index_];
-  }
-
-  Iterator<BufferTy> &operator++() {
-    index_++;
-    return *this;
-  } 
-  Iterator<BufferTy> operator++(int) {
-    Iterator<BufferTy> temp(container_, index_);
-    operator++();
-    return temp;
-  }
-
-  bool operator==(const Iterator<BufferTy> &other) const {
-    return index_ == other.index_;
-  }
-  bool operator!=(const Iterator<BufferTy> &other) const {
-    return !operator==(other);
-  }
-
-private:
-  BufferTy     &container_;
-  std::uint64_t index_;
-};
-
-template <Depth::Tp depth, Color::Tp color> 
-static typename Buffer<depth, color>::template Iterator<Buffer<depth, color>> begin(Buffer<depth, color> &buffer) {
-  return typename Buffer<depth, color>::template Iterator<Buffer<depth, color>>(buffer, 0);
+template <Depth::Tp DepthV,
+          Color::Tp ColorV,
+          bool AlphaSetting>
+auto begin(Buffer<DepthV, ColorV, AlphaSetting> &buffer) {
+  return buffer.Begin();
 }
-template <Depth::Tp depth, Color::Tp color> 
-static typename Buffer<depth, color>::template Iterator<const Buffer<depth, color>> begin(const Buffer<depth, color> &buffer) {
-  return typename Buffer<depth, color>::template Iterator<const Buffer<depth, color>>(buffer, 0);
+template <Depth::Tp DepthV,
+          Color::Tp ColorV,
+          bool AlphaSetting>
+auto begin(const Buffer<DepthV, ColorV, AlphaSetting> &buffer) {
+  return buffer.Begin();
+}
+template <Depth::Tp DepthV,
+          Color::Tp ColorV,
+          bool AlphaSetting>
+auto end(Buffer<DepthV, ColorV, AlphaSetting> &buffer) {
+  return buffer.End();
+}
+template <Depth::Tp DepthV,
+          Color::Tp ColorV,
+          bool AlphaSetting>
+auto end(const Buffer<DepthV, ColorV, AlphaSetting> &buffer) {
+  return buffer.End();
 }
 
-template <Depth::Tp depth, Color::Tp color> 
-static typename Buffer<depth, color>::template Iterator<Buffer<depth, color>> end(Buffer<depth, color> &buffer) {
-  return typename Buffer<depth, color>::template Iterator<Buffer<depth, color>>(buffer, buffer.GetLength());
-}
-template <Depth::Tp depth, Color::Tp color> 
-static typename Buffer<depth, color>::template Iterator<const Buffer<depth, color>> end(const Buffer<depth, color> &buffer) {
-  return typename Buffer<depth, color>::template Iterator<const Buffer<depth, color>>(buffer, buffer.GetLength());
-}
-
-template <Depth::Tp DepthV = Depth::Eight>
-using AlphaChannel = Buffer<DepthV, Color::Grayscale>;
-
-template <Depth::Tp DepthV = Depth::Eight, 
-          Color::Tp ColorV = Color::RGB>
-struct AlphaBuffer {
-  Buffer       <DepthV, ColorV> buffer;
-  AlphaChannel <DepthV>         alpha;
-};
+template <Depth::Tp DepthV = DefDepth,
+          Color::Tp ColorV = DefColor>
+using AlphaBuffer = Buffer<DepthV, ColorV, EnableAlpha>;
 
 };
