8f7beb8337b2630d2f743d057b2ba522c2625a04
weqeq <ggmodwear@gmail.com>
2025-04-08 00:17:34 +0300

    Rewrite depth convertor

--- include/image/convertor/depth.h
+++ include/image/convertor/depth.h
@@ -12,64 +12,88 @@ namespace Image {
 template <typename T>
 class DepthConvertor {
 public:
-
   static_assert(false);
-
   DepthConvertor(T) {}
-
-}; // DepthConvertor<T>
-
-template <Depth::Tp Dh, Color::Tp Cr>
-class DepthConvertor<Element<Dh, Cr>> {
+};
+template <Depth::Tp DepthV, 
+          Color::Tp ColorV,
+          bool AlphaSetting>
+class DepthConvertor<Element<DepthV, ColorV, AlphaSetting>> {
 public:
-  DepthConvertor(const Element<Dh, Cr> &input) : input_(input) {}
+  template <Depth::Tp CustomDepthV>
+  using CustomElement = Element<CustomDepthV, ColorV, AlphaSetting>;
 
-  template <Depth::Tp ODh>
-  std::conditional_t<Dh != ODh,
-          Element<ODh, Cr>,
-    const Element<ODh, Cr> &>
-  Convert() const {
-    if constexpr (Dh == ODh) {
+  using UsedElement = CustomElement<DepthV>;
+
+  template <Depth::Tp CustomDepthV>
+  using CustomValue = typename CustomElement<CustomDepthV>::Value;
+
+  using Value = CustomValue<DepthV>;
+
+  template <Depth::Tp CustomDepthV>
+  static constexpr CustomValue<CustomDepthV> CustomWhite = Depth::Max<CustomDepthV>;
+
+  static constexpr Value White = CustomWhite<DepthV>;
+
+  template <Depth::Tp CustomDepthV>
+  static constexpr CustomValue<CustomDepthV> CustomBlack = Depth::Min<CustomDepthV>;
+
+  static constexpr Value Black = CustomBlack<DepthV>;
+
+  DepthConvertor(const UsedElement &input) : input_(input) {}
+
+  template <Depth::Tp OutputDepthV>
+  decltype(auto) Convert() const {
+    if constexpr (DepthV == OutputDepthV) {
       return input_;
     } else {
-      Element<ODh, Cr> output;
-      auto output_iterator = begin(output);
-      for (const auto &channel : input_) {
-        *output_iterator++ = static_cast<Depth::Underlying<ODh>>(
-          (static_cast<double>(channel) / Depth::Max<Dh>) * Depth::Max<ODh>
+      CustomElement<OutputDepthV> output;
+      for (auto index = 0u;
+                index < Color::ChannelCount<ColorV, DisableAlpha>;
+                index++) {
+        output[index] = static_cast<CustomValue<OutputDepthV>>(
+          (static_cast<double>(input_[index]) / White) * CustomWhite<OutputDepthV>
         );
       }
-      return std::move(output);
+      if constexpr (UsedElement::AlphaEnabled) {
+        output[Color::AlphaIndex<ColorV>] = input_[Color::AlphaIndex<ColorV>];
+      }
+      return output;
     }
   }
 private:
-  const Element<Dh, Cr> &input_;
-
-}; // DepthConvertor<Element<Dh, Cr>>
-
-template <Depth::Tp Dh, Color::Tp Cr>
-class DepthConvertor<Buffer<Dh, Cr>> {
+  const UsedElement &input_;
+};
+template <Depth::Tp DepthV,
+          Color::Tp ColorV,
+          bool AlphaSetting>
+class DepthConvertor<Buffer<DepthV, ColorV, AlphaSetting>> {
 public:
-  DepthConvertor(const Buffer<Dh, Cr> &input) : input_(input) {}
+  template <Depth::Tp CustomDepthV>
+  using CustomBuffer = Buffer<CustomDepthV, ColorV, AlphaSetting>;
 
-  template <Depth::Tp ODh>
-  std::conditional_t<Dh != ODh,
-          Buffer<ODh, Cr>,
-    const Buffer<ODh, Cr> &>
-  Convert() const {
-    if constexpr (Dh == ODh) {
+  using UsedBuffer = CustomBuffer<DepthV>;
+
+  DepthConvertor(const UsedBuffer &input) : input_(input) {}
+
+  template <Depth::Tp OutputDepthV>
+  decltype(auto) Convert() const {
+    if constexpr (DepthV == OutputDepthV) {
       return input_;
     } else {
-      Buffer<ODh, Cr> output(input_.GetRCount(), input_.GetCCount());
-      auto iterator = begin(input_);
-      for (auto &element : output) {
-        element = Image::DepthConvertor(*iterator++).template Convert<ODh>();
+      CustomBuffer<OutputDepthV> output(
+        input_.GetRowCount    (), 
+        input_.GetColumnCount ()
+      );
+      for (auto index = 0u;
+                index < output.GetLength();
+                index++) {
+        output[index] = Image::DepthConvertor(input_[index]).template Convert<OutputDepthV>();
       }
-      return std::move(output);
+      return output;
     }
   }
 private:
-  const Buffer<Dh, Cr> &input_;
-
-}; // DepthConvertor<Buffer<depth, color>>
-}; // Image
+  const UsedBuffer &input_;
+}; 
+};
