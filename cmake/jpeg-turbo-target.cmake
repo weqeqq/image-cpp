1d7f648a5bff5205c5e68658cad797bf0be63e72
weqeq <ggmodwear@gmail.com>
2025-04-21 02:55:24 +0300

    Fix build

--- cmake/jpeg-turbo-target.cmake
+++ cmake/jpeg-turbo-target.cmake
@@ -7,10 +7,14 @@ ExternalProject_Add(
   GIT_PROGRESS true
   PREFIX ${CMAKE_BINARY_DIR}/jpeg-turbo
 
-  CONFIGURE_COMMAND ${CMAKE_COMMAND} -S <SOURCE_DIR> -B <BINARY_DIR> 
-                      -DCMAKE_BUILD_TYPE=Release 
-                      -DENABLE_SHARED=OFF
-                      -DENABLE_STATIC=ON
+  CONFIGURE_COMMAND ${CMAKE_COMMAND} 
+    -S <SOURCE_DIR> 
+    -B <BINARY_DIR> 
+    -DCMAKE_BUILD_TYPE=Release 
+    -DENABLE_SHARED=OFF
+    -DENABLE_STATIC=ON
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_CURRENT_SOURCE_DIR}/${CMAKE_TOOLCHAIN_FILE}
+
   BUILD_COMMAND     ${CMAKE_COMMAND} --build   <BINARY_DIR> --config Release
   INSTALL_COMMAND   ${CMAKE_COMMAND} --install <BINARY_DIR> --prefix <INSTALL_DIR> --config Release)
 
