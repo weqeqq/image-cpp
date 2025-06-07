d5cd062741109ad531442f5e9a53ac5b3e7ce9e1
weqeq <ggmodwear@gmail.com>
2025-06-07 19:23:50 +0300

    Bump file-cpp dependency to latest commit

--- cmake/target/file.cmake
+++ cmake/target/file.cmake
@@ -4,7 +4,7 @@ include(FetchContent)
 FetchContent_Declare(
   file-project
   GIT_REPOSITORY https://github.com/weqeqq/file-cpp.git
-  GIT_TAG        964b7e842aa36b8fa40be0608109029f76062fa1
+  GIT_TAG        06108062077d0d6abd8550988fd8cf5a9f5d3d11
   GIT_PROGRESS   ON
 )
 FetchContent_MakeAvailable(
